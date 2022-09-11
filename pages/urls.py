bd955a4633ac1b5c87b590c58b1e2338deac61f0
aurelaukperaj <109893057+aurelaukperaj@users.noreply.github.com>
2022-09-11 09:25:47 +0200

    Add static information pages

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = "pages" 
 
 urlpatterns = [
- 
+    path("", views.homepage, name="homepage"), 
+    path("about/", views.about_us, name="about"), 
+    path("contact/", views.contact_us, name="contact"), 
 ]
