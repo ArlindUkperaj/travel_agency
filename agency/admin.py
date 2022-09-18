f2e559aeecf9026fd88521b11b6724965f1e8c3d
ArlindUkperaj <ukperajarlind@gmail.com>
2022-09-18 09:14:35 +0200

    i created the classes on models.py

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Continent
+from .models import Country
+from .models import City
+from .models import Airport
+from .models import Hotel
 
-# Register your models here.
+
+admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Airport)
+admin.site.register(Hotel)
--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 
 
@@ -20,3 +21,63 @@ class Country(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=80)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = "cities"
+    
+    def __str__(self):
+        return self.name
+    
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=80)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    stars = models.CharField(max_length=7)
+    description = models.TextField
+    
+    class Meta:
+        db_table = "hotels"
+    
+    def __str__(self):
+        return self.name
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=80)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        db_table = "airport"
+    
+    def __str__(self):
+        return self.name
+    
+    
+# class Trip(models.Model):
+      
+#     # starting_location = models.ForeignKey(City, Airport, on_delete=models.CASCADE)
+#     # destination = models.ForeignKey(City, Airport, Hotel, on_delete=models.CASCADE)
+#     date_of_departure = models.DateField
+#     date_of_return = models.DateField
+#     # number_of_days = days
+#     # type: (BB - bed & breakfast, HB – half board, FB – full board, AI – all inclusive )
+#     # price_for_adult = ?
+#     # price_for_child = ?
+#     # promoted = ?
+#     # number_of_places_adults = ?
+#     # number_of_places_children = ?
+    
+    
+    
+    
+    # def days(date_of_departure, date_of_return):
+    #     if date_of_return:
+    #         return date_of_departure - date_of_return
+    #     else:
+    #         return 'please selecet return date'
